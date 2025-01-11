629134bbe822bc281f32b154bb269f8b802f3822
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-01-11 03:23:50 +0300

    prepare for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ group = "ru.trfx"
 version = "0.0.1"
 
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+    mainClass.set("ru.trfx.catalog.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
